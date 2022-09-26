0395fbe4de406e2d9b45669e9e658ec36fef155c
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-26 20:30:54 +0300

    Добавьте файлы проекта.

---
+++ SeaBattle.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Sea Battle!" << std::endl;
+    bool player1[10][10];
+    bool player2[10][10];
+    std::cout << "Welcome, Admiral!" << std::endl;
+}
